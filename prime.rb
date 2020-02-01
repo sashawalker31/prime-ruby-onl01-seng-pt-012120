aba449fa99f55060a1e8ebef208ca3642a6f606f
sasha walker <sashawalker31@gmail.com>
2020-02-01 02:17:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
-
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
+end
 
 
 
