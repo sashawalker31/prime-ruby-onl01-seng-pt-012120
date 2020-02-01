3170f8eb92f68e45504519da10d9fb51da951e9b
sasha walker <sashawalker31@gmail.com>
2020-02-01 02:17:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
+end
+
+
+
+
+
+ #expect(prime?(2)).to be(true)
+    #expect(prime?(3)).to be(true)
+    #expect(prime?(11)).to be(true)
+    #expect(prime?(105557)).to be(true)
