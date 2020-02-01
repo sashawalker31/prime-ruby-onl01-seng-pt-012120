1723d289b2fdadc41fa8138c6490ed6b4a3b2294
sasha walker <sashawalker31@gmail.com>
2020-02-01 02:08:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,16 @@
 def prime?(numbers)
   (2..n-1).none? {|divisor| n % divisor == 0}
 end
-puts 5.prime?
-puts 
+  puts 5.prime?
+  puts 11.prime?
+  puts 105557.prime?
+end
+
+
+
+
+
+ #expect(prime?(2)).to be(true)
+    #expect(prime?(3)).to be(true)
+    #expect(prime?(11)).to be(true)
+    #expect(prime?(105557)).to be(true)
