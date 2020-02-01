5985d9e4d313899422c70f3723c233361fbbf1ba
sasha walker <sashawalker31@gmail.com>
2020-02-01 02:10:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 def prime?(numbers)
   (2..n-1).none? {|divisor| n % divisor == 0}
-end
   puts Prime.first(105557)
 end
+end
 
 
 
 
 
+ #expect(prime?(2)).to be(true)
+    #expect(prime?(3)).to be(true)
+    #expect(prime?(11)).to be(true)
+    #expect(prime?(105557)).to be(true)
